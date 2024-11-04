85653a85738a2b7e269b8d3294166598ddfcd249
RyanMConstable <RyanMConstable@gmail.com>
2024-11-03 21:13:26 -0500

    Added info.py

---
+++ Info.py
@@ -0,0 +1,20 @@
+from demoparser2 import DemoParser
+
+if __name__ == "__main__":
+    print("Main started")
+    
+    #Testing my local dem file (This can be on git it's not sensitive data)
+    demoFile = "./match730_003716174692134945057_0469028642_129.dem"
+    
+    #Set up the base parser info
+    parser = DemoParser(demoFile)
+    event_df = parser.parse_event("player_death", player=["X", "Y"], other=["total_rounds_played"])
+    ticks_df = parser.parse_ticks(["X", "Y"])
+    
+    #List all events in the game
+    events = parser.list_game_events()
+    
+    #List of lists
+    #The main list contains lists of the format [steam username, steamid]
+    players = parser.parse_event("player_team")
+    print(players[["user_name", "user_steamid"]].values.tolist())
