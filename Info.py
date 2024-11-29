42b4a168d8bd6a7bfa2bd143830f7b3a6ce30c67
RyanMConstable <RyanMConstable@gmail.com>
2024-11-29 12:21:03 -0500

    Added jumps stat

--- Info.py
+++ Info.py
@@ -40,6 +40,7 @@ if __name__ == "__main__":
     reload_df = parser.parse_event("weapon_reload")
     item_pickup_df = parser.parse_event("item_pickup")
     bomb_pickup_df = parser.parse_event("bomb_pickup")
+    jump_df = parser.parse_event("player_jump")
     for player in playerInfo.keys():
         """Player_death_df"""
         #Basic kill stats
@@ -71,9 +72,11 @@ if __name__ == "__main__":
         playerInfo[player]["itemPickups"] = len(item_pickup_df.loc[item_pickup_df["user_steamid"] == player])
         #bomb pickups
         playerInfo[player]["bombPickups"] = len(bomb_pickup_df.loc[bomb_pickup_df["user_steamid"] == player])
+        #Jumps
+        playerInfo[player]["jumps"] = len(jump_df.loc[jump_df["user_steamid"] == player])
      
     #Testing things
-    temp_df = parser.parse_event("bomb_pickup")
+    temp_df = parser.parse_event("player_jump")
     print(f"{temp_df.columns} \n")
     print(temp_df.head)
     
